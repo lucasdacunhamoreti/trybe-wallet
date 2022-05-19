862eba04acbebc6d31d3034a5ec5cea37b5bfca0
Lucas da Cunha Moreti <lucasdacunha00@gmail.com>
2022-05-19 10:50:51 -0300

    Requisito 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -4,31 +4,127 @@ import { connect } from 'react-redux';
 import { getCurrencies } from '../actions';
 
 class Wallet extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      value: '',
+      description: '',
+      currency: 'USD',
+      paymentMethod: 'Dinheiro',
+      category: 'Alimentação',
+    };
+  }
+
   componentDidMount() {
     const { setCurrencies } = this.props;
     setCurrencies();
   }
 
+  handleChange = ({ target }) => {
+    this.setState({ [target.name]: target.value });
+  }
+
   render() {
-    const { email } = this.props;
+    const { email, currencies } = this.props;
+    const { value, description, currency, paymentMethod, category } = this.state;
     return (
-      <header>
-        <p data-testid="email-field">
-          { email }
-        </p>
-        <p data-testid="total-field">
-          0
-        </p>
-        <p data-testid="header-currency-field">
-          BRL
-        </p>
-      </header>
+      <div>
+        <header>
+          <p data-testid="email-field">
+            { email }
+          </p>
+          <p data-testid="total-field">
+            0
+          </p>
+          <p data-testid="header-currency-field">
+            BRL
+          </p>
+        </header>
+
+        <form>
+          <label htmlFor="value">
+            Valor:
+            <input
+              id="value"
+              name="value"
+              value={ value }
+              type="text"
+              data-testid="value-input"
+              onChange={ this.handleChange }
+            />
+          </label>
+
+          <label htmlFor="description">
+            Descrição:
+            <input
+              name="description"
+              type="text"
+              data-testid="description-input"
+              value={ description }
+              onChange={ this.handleChange }
+              id="description"
+            />
+          </label>
+
+          <label htmlFor="currency">
+            Moeda:
+            <select
+              id="currency"
+              name="currency"
+              onChange={ this.handleChange }
+              value={ currency }
+            >
+              { currencies.map((countryCurrency) => (
+                <option
+                  key={ countryCurrency }
+                  value={ countryCurrency }
+                >
+                  { countryCurrency }
+                </option>))}
+            </select>
+          </label>
+
+          <label htmlFor="paymentMethod">
+            Método de Pagamento:
+            <select
+              id="paymentMethod"
+              data-testid="method-input"
+              onChange={ this.handleChange }
+              name="paymentMethod"
+              value={ paymentMethod }
+            >
+              <option value="Dinheiro">Dinheiro</option>
+              <option value="Cartão de crédito">Cartão de crédito</option>
+              <option value="Cartão de débito">Cartão de débito</option>
+            </select>
+          </label>
+
+          <label htmlFor="category">
+            Categoria:
+            <select
+              id="category"
+              data-testid="tag-input"
+              name="category"
+              value={ category }
+              onChange={ this.handleChange }
+            >
+              <option value="Alimentação">Alimentação</option>
+              <option value="Lazer">Lazer</option>
+              <option value="Trabalho">Trabalho</option>
+              <option value="Transporte">Transporte</option>
+              <option value="Saúde">Saúde</option>
+            </select>
+          </label>
+
+        </form>
+      </div>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
   email: state.user.email,
+  currencies: state.wallet.currencies,
 });
 
 const mapDispatchToProps = (dispatch) => ({
@@ -42,4 +138,5 @@ export default connect(mapStateToProps, mapDispatchToProps)(Wallet);
 Wallet.propTypes = {
   email: PropTypes.string.isRequired,
   setCurrencies: PropTypes.func.isRequired,
+  currencies: PropTypes.arrayOf.isRequired,
 };
