ca9080e769a2fa3a78d446e6017c5b5e33785604
Lucas da Cunha Moreti <lucasdacunha00@gmail.com>
2022-07-16 11:06:12 -0300

    Updates

---
+++ precache-manifest.b862f97a1c7cb29318ebef9276b44557.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21b62a5218e790c4acc8de620f5e3c98",
+    "url": "/Projeto-TrybeWallet/index.html"
+  },
+  {
+    "revision": "f982b1c7284e03918461",
+    "url": "/Projeto-TrybeWallet/static/css/main.3d9eb9f9.chunk.css"
+  },
+  {
+    "revision": "8b7e8e2de34188a71849",
+    "url": "/Projeto-TrybeWallet/static/js/2.41750303.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Projeto-TrybeWallet/static/js/2.41750303.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f982b1c7284e03918461",
+    "url": "/Projeto-TrybeWallet/static/js/main.4ea59ddb.chunk.js"
+  },
+  {
+    "revision": "7ae4b0bddf7dc89b7a7d",
+    "url": "/Projeto-TrybeWallet/static/js/runtime-main.a3b0873c.js"
+  },
+  {
+    "revision": "7f611f0fa14dfd6718ccbf8a420a996e",
+    "url": "/Projeto-TrybeWallet/static/media/logo.7f611f0f.svg"
+  }
+]);
